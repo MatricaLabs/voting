bcd1eb7d0cfa95e9a8e0c708073a4c76d6f955a8
yihau <a122092487@gmail.com>
2022-03-05 15:13:55 +0800

    feat: proposer can decide which NFT can vote

--- programs/nft-vote/src/lib.rs
+++ programs/nft-vote/src/lib.rs
@@ -1,5 +1,6 @@
 use anchor_lang::prelude::*;
 use anchor_spl::token::TokenAccount;
+use mpl_token_metadata::{pda::find_metadata_account, state::Metadata};
 
 declare_id!("9rJ3xEnJhff2oJgqXzdrHFshgS5QA1PkNFQp4e1vwzp6");
 
@@ -15,6 +16,7 @@ pub mod nft_vote {
         title: String,
         content: String,
         options: Vec<String>,
+        allowed_creators: Vec<Pubkey>,
         ended_at: i64,
     ) -> ProgramResult {
         // check title length
@@ -28,20 +30,36 @@ pub mod nft_vote {
         proposal.title = title;
         proposal.content = content;
         proposal.options = options;
+        proposal.allowed_creators = allowed_creators;
         proposal.ended_at = ended_at;
         proposal.created_at = Clock::get()?.unix_timestamp;
 
         Ok(())
     }
-
     pub fn vote(ctx: Context<Vote>, option_idx: u8) -> ProgramResult {
         let now = Clock::get()?.unix_timestamp;
+        let proposal = &ctx.accounts.proposal;
 
         // check is a valid voting time
-        require!(
-            ctx.accounts.proposal.ended_at >= now,
-            ErrorCode::ProposalVotingHasClosed,
-        );
+        require!(proposal.ended_at >= now, ErrorCode::ProposalVotingHasClosed,);
+
+        // check creator is verified
+        if proposal.allowed_creators.len() > 0 {
+            let metadata = Metadata::from_account_info(&ctx.accounts.metadata.to_account_info())?;
+            let mut allowed = false;
+            'mainloop: for creator in &metadata.data.creators.unwrap() {
+                if !creator.verified {
+                    continue;
+                }
+                for allowed_creator in &proposal.allowed_creators {
+                    if &creator.address == allowed_creator {
+                        allowed = true;
+                        break 'mainloop;
+                    }
+                }
+            }
+            require!(allowed, ErrorCode::InvalidMint);
+        }
 
         // init vote record
         let vote_record = &mut ctx.accounts.vote_record;
@@ -55,7 +73,7 @@ pub mod nft_vote {
 }
 
 #[derive(Accounts)]
-#[instruction(title: String, content: String, options: Vec<String>)]
+#[instruction(title: String, content: String, options: Vec<String>, allowed_creators: Vec<Pubkey>)]
 pub struct Propose<'info> {
     #[account(mut)]
     pub proposer: Signer<'info>,
@@ -68,6 +86,7 @@ pub struct Propose<'info> {
             4 + MAX_TITLE_LEN +
             4 + content.len() +
             4 + options.iter().fold(0, |total, s| total + 4 + s.len()) +
+            4 + allowed_creators.iter().fold(0, |total, s| total + 32) +
             8 +
             8
     )]
@@ -82,6 +101,8 @@ pub struct Vote<'info> {
     pub owner: Signer<'info>,
     #[account("&token_account.owner == owner.key", "token_account.amount == 1")]
     pub token_account: Account<'info, TokenAccount>,
+    #[account(address = find_metadata_account(&token_account.mint).0)]
+    pub metadata: AccountInfo<'info>,
     #[account(mut, "(option_idx as usize) < proposal.options.len()")]
     pub proposal: Account<'info, Proposal>,
     #[account(
@@ -101,6 +122,7 @@ pub struct Proposal {
     pub title: String,
     pub content: String,
     pub options: Vec<String>,
+    pub allowed_creators: Vec<Pubkey>,
     pub ended_at: i64,
     pub created_at: i64,
 }
@@ -119,4 +141,6 @@ pub enum ErrorCode {
     TitleTooLong,
     #[msg("proposal voting has closed")]
     ProposalVotingHasClosed,
+    #[msg("invalid mint")]
+    InvalidMint,
 }
--- tests/nft-vote.ts
+++ tests/nft-vote.ts
@@ -3,6 +3,11 @@ import { Program } from "@project-serum/anchor";
 import assert from "assert";
 import { NftVote } from "../target/types/nft_vote";
 import { createMintAndVault } from "@project-serum/common";
+import { actions, programs } from "@metaplex/js";
+import { Creator, MetadataDataData } from "@metaplex-foundation/mpl-token-metadata";
+const {
+  metadata: { Metadata },
+} = programs;
 
 describe("nft-vote", () => {
   const provider = anchor.Provider.env();
@@ -23,6 +28,7 @@ describe("nft-vote", () => {
       const content = "this is line1\nthis line 2";
       const options = ["option1", "option2"];
       const endedAt = new anchor.BN(new Date().getTime() / 1000 - 86400);
+      const allowedCreators = [];
 
       await propose({
         proposer: proposer,
@@ -39,6 +45,7 @@ describe("nft-vote", () => {
       assert.equal(proposalAccountInfo.title, title);
       assert.equal(proposalAccountInfo.content, content);
       assert.deepEqual(proposalAccountInfo.options, options);
+      assert.deepEqual(proposalAccountInfo.allowedCreators, allowedCreators);
       assert.ok(proposalAccountInfo.createdAt > new anchor.BN(0));
       assert.ok(
         proposalAccountInfo.createdAt <= timeAfterProposing,
@@ -56,14 +63,215 @@ describe("nft-vote", () => {
         assert.equal(err.toString(), "title should less than or equals to 100");
       }
     });
+
+    it("propose with one creator", async () => {
+      const proposer = anchor.web3.Keypair.generate();
+      await provider.connection.confirmTransaction(
+        await provider.connection.requestAirdrop(proposer.publicKey, 10 * anchor.web3.LAMPORTS_PER_SOL),
+        "processed"
+      );
+
+      const proposal = anchor.web3.Keypair.generate();
+      const title = "test title?";
+      const content = "this is line1\nthis line 2";
+      const options = ["option1", "option2"];
+      const endedAt = new anchor.BN(new Date().getTime() / 1000 - 86400);
+      const randomCreator = anchor.web3.Keypair.generate().publicKey;
+      const allowedCreators = [randomCreator];
+
+      await propose({
+        proposer: proposer,
+        proposal: proposal,
+        title: title,
+        content: content,
+        options: options,
+        endedAt: endedAt,
+        allowedCreators: allowedCreators,
+      });
+      let timeAfterProposing = new anchor.BN(new Date().getTime() / 1000);
+
+      const proposalAccountInfo = await program.account.proposal.fetch(proposal.publicKey);
+      assert.equal(proposalAccountInfo.proposer.toBase58(), proposer.publicKey.toBase58());
+      assert.equal(proposalAccountInfo.title, title);
+      assert.equal(proposalAccountInfo.content, content);
+      assert.deepEqual(proposalAccountInfo.options, options);
+      assert.deepEqual(proposalAccountInfo.allowedCreators, allowedCreators);
+      assert.ok(proposalAccountInfo.createdAt > new anchor.BN(0));
+      assert.ok(
+        proposalAccountInfo.createdAt <= timeAfterProposing,
+        `createdAt: ${proposalAccountInfo.createdAt}, timeAfterProposing: ${timeAfterProposing}`
+      );
+    });
+
+    it("propose with two creator", async () => {
+      const proposer = anchor.web3.Keypair.generate();
+      await provider.connection.confirmTransaction(
+        await provider.connection.requestAirdrop(proposer.publicKey, 10 * anchor.web3.LAMPORTS_PER_SOL),
+        "processed"
+      );
+
+      const proposal = anchor.web3.Keypair.generate();
+      const title = "test title?";
+      const content = "this is line1\nthis line 2";
+      const options = ["option1", "option2"];
+      const endedAt = new anchor.BN(new Date().getTime() / 1000 - 86400);
+      const randomCreator1 = anchor.web3.Keypair.generate().publicKey;
+      const randomCreator2 = anchor.web3.Keypair.generate().publicKey;
+      const allowedCreators = [randomCreator1, randomCreator2];
+
+      await propose({
+        proposer: proposer,
+        proposal: proposal,
+        title: title,
+        content: content,
+        options: options,
+        endedAt: endedAt,
+        allowedCreators: allowedCreators,
+      });
+      let timeAfterProposing = new anchor.BN(new Date().getTime() / 1000);
+
+      const proposalAccountInfo = await program.account.proposal.fetch(proposal.publicKey);
+      assert.equal(proposalAccountInfo.proposer.toBase58(), proposer.publicKey.toBase58());
+      assert.equal(proposalAccountInfo.title, title);
+      assert.equal(proposalAccountInfo.content, content);
+      assert.deepEqual(proposalAccountInfo.options, options);
+      assert.deepEqual(proposalAccountInfo.allowedCreators, allowedCreators);
+      assert.ok(proposalAccountInfo.createdAt > new anchor.BN(0));
+      assert.ok(
+        proposalAccountInfo.createdAt <= timeAfterProposing,
+        `createdAt: ${proposalAccountInfo.createdAt}, timeAfterProposing: ${timeAfterProposing}`
+      );
+    });
   });
 
   describe("vote", () => {
-    it("a normal vote", async () => {
+    it("vote to a unlimited proposal with a random NFT without creators", async () => {
       const proposalPubkey = await propose();
 
+      let owner = await newKeypairWithLamports(10 * anchor.web3.LAMPORTS_PER_SOL);
+      let { mintPubkey: mintPubkey, tokenAccountPubkey: tokenAccountPubkey } = await mintNFT(owner);
+
       const optionIdx = 1;
-      const { mintPubkey, voteRecordPubkey } = await vote(proposalPubkey, optionIdx);
+
+      const voteRecordPubkey = await vote(proposalPubkey, optionIdx, {
+        owner: owner,
+        mint: mintPubkey,
+        tokenAccount: tokenAccountPubkey,
+      });
+      const timeAfterVoting = new anchor.BN(new Date().getTime() / 1000);
+
+      const voteRecord = await program.account.voteRecord.fetch(voteRecordPubkey);
+      assert.equal(voteRecord.proposal.toBase58(), proposalPubkey.toBase58());
+      assert.equal(voteRecord.mint.toBase58(), mintPubkey.toBase58());
+      assert.equal(voteRecord.optionIdx, optionIdx);
+      assert.ok(voteRecord.createdAt > new anchor.BN(0));
+      assert.ok(
+        voteRecord.createdAt <= timeAfterVoting,
+        `createdAt: ${voteRecord.createdAt}, timeAfterVoting: ${timeAfterVoting}`
+      );
+    });
+
+    it("vote to a unlimited proposal with a random NFT with creators", async () => {
+      const proposalPubkey = await propose();
+
+      let owner = await newKeypairWithLamports(10 * anchor.web3.LAMPORTS_PER_SOL);
+      let { mintPubkey: mintPubkey, tokenAccountPubkey: tokenAccountPubkey } = await mintNFT(owner, [
+        new Creator({
+          address: provider.wallet.publicKey.toBase58(),
+          verified: true,
+          share: 100,
+        }),
+      ]);
+
+      const optionIdx = 1;
+
+      const voteRecordPubkey = await vote(proposalPubkey, optionIdx, {
+        owner: owner,
+        mint: mintPubkey,
+        tokenAccount: tokenAccountPubkey,
+      });
+      const timeAfterVoting = new anchor.BN(new Date().getTime() / 1000);
+
+      const voteRecord = await program.account.voteRecord.fetch(voteRecordPubkey);
+      assert.equal(voteRecord.proposal.toBase58(), proposalPubkey.toBase58());
+      assert.equal(voteRecord.mint.toBase58(), mintPubkey.toBase58());
+      assert.equal(voteRecord.optionIdx, optionIdx);
+      assert.ok(voteRecord.createdAt > new anchor.BN(0));
+      assert.ok(
+        voteRecord.createdAt <= timeAfterVoting,
+        `createdAt: ${voteRecord.createdAt}, timeAfterVoting: ${timeAfterVoting}`
+      );
+    });
+
+    it("vote to a limited proposal with a random NFT", async () => {
+      let randomCreator = anchor.web3.Keypair.generate();
+      const proposalPubkey = await propose({ allowedCreators: [randomCreator.publicKey] });
+
+      let owner = await newKeypairWithLamports(10 * anchor.web3.LAMPORTS_PER_SOL);
+      let { mintPubkey: mintPubkey, tokenAccountPubkey: tokenAccountPubkey } = await mintNFT(owner, [
+        new Creator({
+          address: provider.wallet.publicKey.toBase58(),
+          verified: true,
+          share: 100,
+        }),
+      ]);
+
+      try {
+        await vote(proposalPubkey, 1, {
+          owner: owner,
+          mint: mintPubkey,
+          tokenAccount: tokenAccountPubkey,
+        });
+        assert.ok(false);
+      } catch (err) {
+        assert.equal(err.toString(), "invalid mint");
+      }
+    });
+
+    it("vote to a limited proposal with a falsify NFT", async () => {
+      const proposalPubkey = await propose({ allowedCreators: [provider.wallet.publicKey] });
+
+      let owner = await newKeypairWithLamports(10 * anchor.web3.LAMPORTS_PER_SOL);
+      let { mintPubkey: mintPubkey, tokenAccountPubkey: tokenAccountPubkey } = await mintNFT(owner, [
+        new Creator({
+          address: provider.wallet.publicKey.toBase58(),
+          verified: false,
+          share: 100,
+        }),
+      ]);
+
+      try {
+        await vote(proposalPubkey, 1, {
+          owner: owner,
+          mint: mintPubkey,
+          tokenAccount: tokenAccountPubkey,
+        });
+        assert.ok(false);
+      } catch (err) {
+        assert.equal(err.toString(), "invalid mint");
+      }
+    });
+
+    it("vote to a limited proposal with a corresponded NFT", async () => {
+      const proposalPubkey = await propose({ allowedCreators: [provider.wallet.publicKey] });
+
+      let owner = await newKeypairWithLamports(10 * anchor.web3.LAMPORTS_PER_SOL);
+      let { mintPubkey: mintPubkey, tokenAccountPubkey: tokenAccountPubkey } = await mintNFT(owner, [
+        new Creator({
+          address: provider.wallet.publicKey.toBase58(),
+          verified: true,
+          share: 100,
+        }),
+      ]);
+
+      const optionIdx = 1;
+
+      const voteRecordPubkey = await vote(proposalPubkey, optionIdx, {
+        owner: owner,
+        mint: mintPubkey,
+        tokenAccount: tokenAccountPubkey,
+      });
+
       const timeAfterVoting = new anchor.BN(new Date().getTime() / 1000);
 
       const voteRecord = await program.account.voteRecord.fetch(voteRecordPubkey);
@@ -89,7 +297,6 @@ describe("nft-vote", () => {
 
       try {
         await vote(proposalPubkey, 1, { owner: owner, mint: mintPubkey, tokenAccount: tokenAccount });
-        await vote(proposalPubkey, 1, { owner: owner, mint: mintPubkey, tokenAccount: tokenAccount });
         assert.ok(false);
       } catch (err) {
         //TODO check error
@@ -99,8 +306,11 @@ describe("nft-vote", () => {
     it("vote to a closed proposal", async () => {
       const proposalPubkey = await propose({ endedAt: new anchor.BN(new Date().getTime() / 1000 - 86400) });
 
+      let owner = await newKeypairWithLamports(10 * anchor.web3.LAMPORTS_PER_SOL);
+      let { mintPubkey: mintPubkey, tokenAccountPubkey: tokenAccountPubkey } = await mintNFT(owner);
+
       try {
-        await vote(proposalPubkey, 1);
+        await vote(proposalPubkey, 1, { owner: owner, mint: mintPubkey, tokenAccount: tokenAccountPubkey });
         assert.ok(false);
       } catch (err) {
         assert.equal(err.toString(), "proposal voting has closed");
@@ -111,31 +321,22 @@ describe("nft-vote", () => {
   const vote = async (
     proposal: anchor.web3.PublicKey,
     optionIdx: number,
-    param?: {
+    param: {
       owner: anchor.web3.Keypair;
       mint: anchor.web3.PublicKey;
       tokenAccount: anchor.web3.PublicKey;
     }
-  ): Promise<{ voteRecordPubkey: anchor.web3.PublicKey; mintPubkey: anchor.web3.PublicKey }> => {
-    // prepare param
-    if (!param) {
-      const owner = anchor.web3.Keypair.generate();
-      await provider.connection.confirmTransaction(
-        await provider.connection.requestAirdrop(owner.publicKey, 10 * anchor.web3.LAMPORTS_PER_SOL),
-        "processed"
-      );
-      const [mintPubkey, tokenAccount] = await createMintAndVault(provider, new anchor.BN(1), owner.publicKey, 0);
-      param = { owner: owner, mint: mintPubkey, tokenAccount: tokenAccount };
-    }
-
+  ): Promise<anchor.web3.PublicKey> => {
     const [voteRecordPubkey] = await anchor.web3.PublicKey.findProgramAddress(
       [param.mint.toBuffer(), proposal.toBuffer()],
       program.programId
     );
+
     await program.rpc.vote(optionIdx, {
       accounts: {
         owner: param.owner.publicKey,
         tokenAccount: param.tokenAccount,
+        metadata: await Metadata.getPDA(param.mint),
         proposal: proposal,
         voteRecord: voteRecordPubkey,
         systemProgram: anchor.web3.SystemProgram.programId,
@@ -143,7 +344,7 @@ describe("nft-vote", () => {
       signers: [param.owner],
     });
 
-    return { voteRecordPubkey: voteRecordPubkey, mintPubkey: param.mint };
+    return voteRecordPubkey;
   };
 
   const propose = async (param?: {
@@ -153,14 +354,14 @@ describe("nft-vote", () => {
     content?: string;
     options?: Array<string>;
     endedAt?: anchor.BN;
+    allowedCreators?: Array<anchor.web3.PublicKey>;
   }): Promise<anchor.web3.PublicKey> => {
     // prepare param
     param = param || {};
     if (!param.proposer) {
       param.proposer = anchor.web3.Keypair.generate();
       await provider.connection.confirmTransaction(
-        await provider.connection.requestAirdrop(param.proposer.publicKey, 10 * anchor.web3.LAMPORTS_PER_SOL),
-        "processed"
+        await provider.connection.requestAirdrop(param.proposer.publicKey, 10 * anchor.web3.LAMPORTS_PER_SOL)
       );
     }
     param.proposal = param.proposal || anchor.web3.Keypair.generate();
@@ -168,9 +369,10 @@ describe("nft-vote", () => {
     param.content = param.content || "this is line1\nthis line 2";
     param.options = param.options || ["options1", "options2"];
     param.endedAt = param.endedAt || new anchor.BN(new Date().getTime() / 1000 + 86400);
+    param.allowedCreators = param.allowedCreators || [];
 
     // propose
-    await program.rpc.propose(param.title, param.content, param.options, param.endedAt, {
+    await program.rpc.propose(param.title, param.content, param.options, param.allowedCreators, param.endedAt, {
       accounts: {
         proposer: param.proposer.publicKey,
         proposal: param.proposal.publicKey,
@@ -181,5 +383,47 @@ describe("nft-vote", () => {
 
     // return the proposal key
     return param.proposal.publicKey;
+  };
+
+  // use provider.wallet to mint nft
+  // if don't give any creators, it use provider.wallet as a default creator
+  const mintNFT = async (
+    owner: anchor.web3.Keypair,
+    creators?: Array<Creator>
+  ): Promise<{
+    mintPubkey: anchor.web3.PublicKey;
+    tokenAccountPubkey: anchor.web3.PublicKey;
+  }> => {
+    const [mintPubkey, tokenAccountPubkey] = await createMintAndVault(provider, new anchor.BN(1), owner.publicKey, 0);
+    const metadataData = new MetadataDataData({
+      name: "Test",
+      symbol: "",
+      uri: "",
+      sellerFeeBasisPoints: 100,
+      creators: creators,
+    });
+
+    await provider.connection.confirmTransaction(
+      await actions.createMetadata({
+        connection: provider.connection,
+        wallet: provider.wallet,
+        editionMint: mintPubkey,
+        metadataData: metadataData,
+      })
+    );
+
+    return {
+      mintPubkey,
+      tokenAccountPubkey,
+    };
+  };
+
+  const newKeypairWithLamports = async (lamports: number): Promise<anchor.web3.Keypair> => {
+    let kp = anchor.web3.Keypair.generate();
+    await provider.connection.confirmTransaction(
+      await provider.connection.requestAirdrop(kp.publicKey, lamports),
+      "processed"
+    );
+    return kp;
   };
 });
