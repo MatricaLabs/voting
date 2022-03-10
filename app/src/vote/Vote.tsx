97988a34c93614f2fccb82a7208586a483d212cf
yihau <a122092487@gmail.com>
2022-03-10 14:27:49 +0800

    feat: allow vote with multiple NFTs

--- app/src/vote/Vote.tsx
+++ app/src/vote/Vote.tsx
@@ -14,7 +14,7 @@ export default (() => {
   const { connection } = useConnection();
   const anchorWallet = useAnchorWallet();
 
-  const [mintAddress, setMintAddress] = useState("");
+  const [mintAddressesInput, setMintAddressesInput] = useState("");
   const [proposalAddress, setProposalAddress] = useState("");
   const [optionIdx, setOptionIdx] = useState("");
 
@@ -26,50 +26,77 @@ export default (() => {
     const program = getProgram(connection, anchorWallet);
 
     try {
-      let mintPubkey = new web3.PublicKey(mintAddress);
-      let tokenAccountPubkey = await Token.getAssociatedTokenAddress(
-        ASSOCIATED_TOKEN_PROGRAM_ID,
-        TOKEN_PROGRAM_ID,
-        mintPubkey,
-        anchorWallet.publicKey
-      );
-      let proposalPubkey = new web3.PublicKey(proposalAddress);
+      let max = 7; // a tx can include 7 NFTs vote (approx.)
+      let mintAddresses = mintAddressesInput.split(",");
+      console.log(mintAddresses)
 
-      const [voteRecordPubkey] = await web3.PublicKey.findProgramAddress(
-        [mintPubkey.toBuffer(), proposalPubkey.toBuffer()],
-        program.programId
-      );
+      let txs: Array<web3.Transaction> = [];
 
-      let tx = program.transaction.vote(parseInt(optionIdx), {
-        accounts: {
-          owner: anchorWallet.publicKey,
-          tokenAccount: tokenAccountPubkey,
-          metadata: await Metadata.getPDA(mintPubkey),
-          proposal: proposalPubkey,
-          voteRecord: voteRecordPubkey,
-          systemProgram: web3.SystemProgram.programId,
-        },
-      });
-      tx.recentBlockhash = (await connection.getRecentBlockhash()).blockhash;
-      tx.feePayer = anchorWallet.publicKey;
+      for (let txIdx = 0; txIdx < Math.ceil(mintAddresses.length / max); txIdx++) {
+        let tx = new web3.Transaction();
 
-      let signedTx = await anchorWallet.signTransaction(tx);
-      const signature = await connection.sendRawTransaction(signedTx.serialize());
-      await connection.confirmTransaction(signature);
-      setResult(signature);
+        for (
+          let mintAddressIdx = txIdx * max;
+          mintAddressIdx < Math.min((txIdx + 1) * max, mintAddresses.length);
+          mintAddressIdx++
+        ) {
+          let mintAddress = mintAddresses[mintAddressIdx];
+          let mintPubkey = new web3.PublicKey(mintAddress);
+          let tokenAccountPubkey = await Token.getAssociatedTokenAddress(
+            ASSOCIATED_TOKEN_PROGRAM_ID,
+            TOKEN_PROGRAM_ID,
+            mintPubkey,
+            anchorWallet.publicKey
+          );
+          let proposalPubkey = new web3.PublicKey(proposalAddress);
+
+          const [voteRecordPubkey] = await web3.PublicKey.findProgramAddress(
+            [mintPubkey.toBuffer(), proposalPubkey.toBuffer()],
+            program.programId
+          );
+
+          tx.add(
+            program.instruction.vote(parseInt(optionIdx), {
+              accounts: {
+                owner: anchorWallet.publicKey,
+                tokenAccount: tokenAccountPubkey,
+                metadata: await Metadata.getPDA(mintPubkey),
+                proposal: proposalPubkey,
+                voteRecord: voteRecordPubkey,
+                systemProgram: web3.SystemProgram.programId,
+              },
+            })
+          );
+        }
+
+        tx.recentBlockhash = (await connection.getRecentBlockhash()).blockhash;
+        tx.feePayer = anchorWallet.publicKey;
+
+        txs.push(tx);
+      }
+
+      let signedTxs = await anchorWallet.signAllTransactions(txs);
+
+      let signatures = [];
+      for (let i = 0; i < signedTxs.length; i++) {
+        let signedTx = signedTxs[i];
+        signatures.push(await connection.sendRawTransaction(signedTx.serialize()));
+      }
+
+      setResult(JSON.stringify(signatures, null, 2));
     } catch (e) {
       setResult((e as Error).message);
     }
-  }, [connection, anchorWallet, mintAddress, proposalAddress, optionIdx]);
+  }, [connection, anchorWallet, mintAddressesInput, proposalAddress, optionIdx]);
 
   return (
     <div>
       <input
         style={{ width: "200px" }}
         type="text"
-        value={mintAddress}
-        onChange={(v) => setMintAddress(v.target.value)}
-        placeholder="mint address (base58)"
+        value={mintAddressesInput}
+        onChange={(v) => setMintAddressesInput(v.target.value)}
+        placeholder="mint address, use , for multiple NFTs"
       ></input>
       <input
         style={{ width: "200px" }}
